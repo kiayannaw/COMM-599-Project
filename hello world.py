06d21cd44fc75fd053c8d379282d4ec99881bda5
kiayannaw <62769699+kiayannaw@users.noreply.github.com>
2020-03-30 14:45:54 -0400

    Add files via upload

---
+++ hello world.py
@@ -0,0 +1,6 @@
+Python 3.7.7 (v3.7.7:d7c567b08f, Mar 10 2020, 02:56:16) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("Hello world")
+Hello world
+>>> 
